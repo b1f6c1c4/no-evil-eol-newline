39ce0615cbe70a9d81d86a7d2e27e30f055d4760
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-03-18 15:05:45 +0800

    Init

---
+++ spec/no-evil-eol-newline-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NoEvilEolNewlineView from '../lib/no-evil-eol-newline-view';
+
+describe('NoEvilEolNewlineView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
