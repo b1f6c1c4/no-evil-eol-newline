28ba9bbfd96431ff57a7c5074c1cafd7bc43524d
b1f6c1c4 <b1f6c1c4@gmail.com>
2018-03-18 18:15:31 +0800

    Implement func

--- spec/no-evil-eol-newline-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import NoEvilEolNewlineView from '../lib/no-evil-eol-newline-view';
-
-describe('NoEvilEolNewlineView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
